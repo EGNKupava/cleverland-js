92083d1426f5eb7565bb588d889586d044d8a139
EGNKupava <kupavaegn@gmail.com>
2021-03-19 22:21:22 +0300

    yo

--- lession-9/script.js
+++ lession-9/script.js
@@ -8,30 +8,28 @@ function Dog(name) {
   };
 }
 
-function Cat(name) {
-  this.name = name;
-  this.color = "white";
-}
-
 const dogBarbos = new Dog("BARBOS");
-const catKesha = new Cat("Kesha");
 
 let dogBobik = {
   name: "BOBIK",
   color: "grey",
   bark: function () {
-    alert(`${this.name} сказал  BARK!!!`);
+    alert(`${this.name} сказал  Привет`);
   },
 };
+
+function Cat(name) {
+  this.name = name;
+  this.color = "white";
+}
+
+const catKesha = new Cat("Kesha");
 
 let catBarsik = {
   name: "BARSIK",
   color: "white",
-  bark: function () {
-    alert(`${this.name} say  BARK!!!`);
-  },
 };
 
+dogBarbos.bark();
 dogBarbos.bark.call(catKesha); //Вызов от имени catBarsik
-dog.bark.call(dogBobik); //Вызов от имени catBarsik
-dog.bark.call(catBarsik); //Вызов от имени catBarsik
+dogBobik.bark.call(catBarsik); //Вызов от имени catBarsik
