1c368e6b36f57a3884a98447ac2301fac0c03374
EGNKupava <kupavaegn@gmail.com>
2021-03-19 22:16:45 +0300

    контест вызова

---
+++ lession-9/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+function Dog(name) {
+  this.name = name;
+  this.color = "dark";
+  this.bark = function () {
+    alert(`${this.name} сказал  BARK!!!`);
+  };
+}
+
+function Cat(name) {
+  this.name = name;
+  this.color = "white";
+}
+
+const dogBarbos = new Dog("BARBOS");
+const catKesha = new Cat("Kesha");
+
+let dogBobik = {
+  name: "BOBIK",
+  color: "grey",
+  bark: function () {
+    alert(`${this.name} сказал  BARK!!!`);
+  },
+};
+
+let catBarsik = {
+  name: "BARSIK",
+  color: "white",
+  bark: function () {
+    alert(`${this.name} say  BARK!!!`);
+  },
+};
+
+dogBarbos.bark.call(catKesha); //Вызов от имени catBarsik
+dog.bark.call(dogBobik); //Вызов от имени catBarsik
+dog.bark.call(catBarsik); //Вызов от имени catBarsik
+dog.bark.call(catBarsik); //Вызов от имени catBarsik
