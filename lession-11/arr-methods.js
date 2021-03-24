29f990cc309a24c72b55c7f5a215faa6d5822b30
EGNKupava <kupavaegn@gmail.com>
2021-03-24 14:27:06 +0300

    Массивы

---
+++ lession-11/arr-methods.js
@@ -0,0 +1,110 @@
+"use strict";
+const button11 = document.getElementById("11");
+const button12 = document.getElementById("12");
+const button13 = document.getElementById("13");
+const button14 = document.getElementById("14");
+const button15 = document.getElementById("15");
+// const button16 = document.getElementById("16");
+// const button17 = document.getElementById("17");
+// const button18 = document.getElementById("18");
+
+button11.addEventListener("click", spliceArr);
+button12.addEventListener("click", sliceArr);
+button13.addEventListener("click", concatArr);
+button14.addEventListener("click", forEachArr);
+button15.addEventListener("click", reduceArr);
+// button16.addEventListener("click", arrToString);
+
+const animals = ["Кот", "Черепаха", "Собакен", "Попугай"];
+
+function spliceArr() {
+  //arr.splice(index[, deleteCount, elem1, ..., elemN])
+  console.log("animals: ", animals);
+  animals.splice(1, 1);
+  // animals.splice(1, 1, "Жираф", "Крокодил");
+  console.log("animals: ", animals);
+}
+//Метод splice также может вставлять элементы без удаления, для этого достаточно установить deleteCount в 0:
+
+function sliceArr() {
+  console.log("animals: ", animals);
+  const newAnimals = animals.slice(2, 4);
+  console.log("newAnimals: ", newAnimals);
+}
+
+//Можно вызвать slice и вообще без аргументов: arr.slice() создаёт копию массива arr.
+//Это часто используют, чтобы создать копию массива для дальнейших преобразований, которые не должны менять исходный массив.
+
+function concatArr() {
+  console.log("animals: ", animals);
+  const bigAnimal = ["Слон", "Носорог"];
+  const resultArr = animals.concat(bigAnimal, ["Шиншила"], "Кошка");
+  console.log("resultArr: ", resultArr);
+}
+
+function forEachArr() {
+  animals.forEach((item, index, array) => {
+    console.log("item: ", item);
+    console.log("index: ", index);
+    console.log("array: ", array);
+  });
+}
+
+function searchArr() {
+  let arr = [1, 0, false];
+
+  alert(arr.indexOf(0)); // 1
+  alert(arr.indexOf(false)); // 2
+  alert(arr.indexOf(null)); // -1
+
+  alert(arr.includes(1)); // true
+}
+
+//find и findIndex
+// const res = arr.find(function (item, index, array) {
+// если true - возвращается текущий элемент и перебор прерывается
+// если все итерации оказались ложными, возвращается undefined
+// });
+
+// filter
+// const resul = arr.filter(function (item, index, array) {
+// если true - элемент добавляется к результату, и перебор продолжается
+// возвращается пустой массив в случае, если ничего не найдено
+// });
+
+//map
+// const result = arr.map(function (item, index, array) {
+// возвращается новое значение вместо элемента
+// });
+
+function compareNumeric(a, b) {
+  if (a > b) return 1;
+  if (a == b) return 0;
+  if (a < b) return -1;
+}
+
+// let arr = [1, 2, 15];
+// arr.sort((a, b) => {
+//   if (a > b) return 1;
+//   if (a == b) return 0;
+//   if (a < b) return -1;
+// });
+
+//reverse
+// Метод arr.reverse меняет порядок элементов в arr на обратный.
+
+//split и join
+// let names = 'Вася, Петя, Маша';
+// let arr = names.split(', ');
+
+const number = [2, 4, 6, 8, 10, 12, 14];
+const initial = 1;
+
+function reduceArr() {
+  let result = number.reduce((previousValue, item, index, array) => {
+    return item + previousValue;
+  }, initial);
+  console.log("result: ", result);
+}
+
+// Array.isArray
---
+++ lession-11/array.js
@@ -0,0 +1,105 @@
+"use strict";
+const button1 = document.getElementById("1");
+const button2 = document.getElementById("2");
+const button3 = document.getElementById("3");
+const button4 = document.getElementById("4");
+const button5 = document.getElementById("5");
+const button6 = document.getElementById("6");
+const button7 = document.getElementById("7");
+const button8 = document.getElementById("8");
+
+button1.addEventListener("click", addItem);
+button2.addEventListener("click", deleteLastItem);
+button3.addEventListener("click", addFirstItem);
+button4.addEventListener("click", deleteFirstItem);
+
+button5.addEventListener("click", iterateArr);
+button6.addEventListener("click", arrToString);
+// button7.addEventListener("click", deleteFirstItem);
+// button8.addEventListener("click", deleteFirstItem);
+//--------------------------------------------------
+
+const cards = new Array();
+const users = ["Алексей", "Иван", "Николай", "Григорий"];
+console.log("users: ", users);
+console.log("users.length: ", users.length); // Длина массива
+
+// Обращение к элементу с n индексом
+console.log("users[1]: ", users[1]);
+
+const usersData = [
+  {
+    name: "Алексей",
+    age: 18,
+  },
+  {
+    name: "Иван",
+    age: 18,
+  },
+  {
+    name: "Николай",
+    age: 18,
+  },
+  {
+    name: "Григорий",
+    age: 18,
+  },
+];
+
+// Методы массивов
+function addItem() {
+  users.push("Андрей");
+  console.log("users: ", users);
+}
+
+function deleteLastItem() {
+  const lastUser = users.pop();
+  console.log("lastUser: ", lastUser);
+  console.log("users: ", users);
+}
+
+function addFirstItem() {
+  users.unshift("Денис");
+  console.log("users: ", users);
+}
+
+function deleteFirstItem() {
+  const firsUser = users.shift("Денис");
+  console.log("firsUser: ", firsUser);
+  console.log("users: ", users);
+}
+
+// Массив является объектом и, следовательно, ведёт себя как объект.
+// Он копируется по ссылке
+//Массивы - тоже объекты, но так делать нельзя
+
+// users.age = 255;
+// console.log("users: ", users);
+
+function iterateArr() {
+  let arr = ["Яблоко", "Апельсин", "Груша"];
+  for (let i = 0; i < arr.length; i++) {
+    alert(arr[i]);
+  }
+}
+
+// Многомерные массивы
+let matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+console.log("matrix[1][1]", matrix[1][1]);
+
+// toString
+// Массивы по-своему реализуют метод toString, который возвращает список элементов, разделённых запятыми.
+
+function arrToString() {
+  const arr = [1, 2, 3];
+  console.log("arr: ", String(arr));
+}
+
+//Копирование спред оператором
+const users2 = [...users, "BOB"];
+// console.log("users2: ", users2);
